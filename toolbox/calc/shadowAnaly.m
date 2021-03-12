26c76ce55c9a3974b7a5c51c5966f3192c50b07e
LSinpetru <68545459+LSinpetru@users.noreply.github.com>
2021-03-12 15:51:12 +0000

    Update shadowAnaly.m

--- toolbox/calc/shadowAnaly.m
+++ toolbox/calc/shadowAnaly.m
@@ -10,12 +10,30 @@ function [ shadPan ] = shadowAnaly( x, y, z, barC, delta, L_gw)
 %       L_gw    : Rotation matrix from wind to geometric coordinate frame
 %
 % Outputs:
-%       shadPan : Flag for shadowed mesh elements (1xN)
+%       shadPan : Vector containing indices of the panels which are shadowed.
 %
 % Author: David Mostaza-Prieto
 % The University of Manchester
 % November 2012
 %
+%--- Copyright notice ---%
+% Copyright (C) 2021 The University of Manchester
+% Written by David Mostaza Prieto,  Nicholas H. Crisp, Luciana Sinpetru and Sabrina Livadiotti
+%
+% This file is part of the ADBSat toolkit.
+%
+% This program is free software: you can redistribute it and/or modify it
+% under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or (at
+% your option) any later version.
+%
+% This program is distributed in the hope that it will be useful, but
+% WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+% Public License for more details.
+%
+% You should have received a copy of the GNU General Public License along
+% with this program. If not, see <http://www.gnu.org/licenses/>.
 %------------- BEGIN CODE --------------
 
 pAw = L_gw'*[x(1,:);y(1,:);z(1,:)];
