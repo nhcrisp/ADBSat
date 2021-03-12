5c636dda52eb84b52973f03c403ca823bf64806a
LSinpetru <68545459+LSinpetru@users.noreply.github.com>
2021-03-12 16:49:48 +0000

    Update coeff_cook.m

--- toolbox/fmf_eq/coeff_cook.m
+++ toolbox/fmf_eq/coeff_cook.m
@@ -21,6 +21,24 @@ function [cp, ctau, cd, cl] = coeff_cook(param_eq, delta)
 % The University of Manchester
 % September 2012
 %
+%--- Copyright notice ---%
+% Copyright (C) 2021 The University of Manchester
+% Written by David Mostaza Prieto,  Nicholas H. Crisp, Luciana Sinpetru and Sabrina Livadiotti
+%
+% This file is part of the ADBSat toolkit.
+%
+% This program is free software: you can redistribute it and/or modify it
+% under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or (at
+% your option) any later version.
+%
+% This program is distributed in the hope that it will be useful, but
+% WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+% Public License for more details.
+%
+% You should have received a copy of the GNU General Public License along
+% with this program. If not, see <http://www.gnu.org/licenses/>.
 %------------- BEGIN CODE --------------
 
 alpha = param_eq.alpha;
