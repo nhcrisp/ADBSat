01e955490b4ca2d393a09f1da2dd39dc8c54cde6
LSinpetru <68545459+LSinpetru@users.noreply.github.com>
2021-03-12 15:04:43 +0000

    Update MainImport.m

--- examples/MainImport.m
+++ examples/MainImport.m
@@ -7,6 +7,26 @@
 %     SurfN: Surface normals 
 %     BariC: Surfaces baricenters 
 %      Lref: Refrence longitude 
+%
+%--- Copyright notice ---%
+% Copyright (C) 2021 The University of Manchester
+% Written by David Mostaza Prieto,  Nicholas H. Crisp, Luciana Sinpetru and Sabrina Livadiotti
+%
+% This file is part of the ADBSat toolkit.
+%
+% This program is free software: you can redistribute it and/or modify it
+% under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or (at
+% your option) any later version.
+%
+% This program is distributed in the hope that it will be useful, but
+% WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+% Public License for more details.
+%
+% You should have received a copy of the GNU General Public License along
+% with this program. If not, see <http://www.gnu.org/licenses/>.
+%----------- BEGIN CODE ------------%
 
 filename   = 'cube.obj'; % Input: Name of the file in /inou/obj_files
 
@@ -24,3 +44,4 @@ struName = name;     % Output: Name of the matlab file mesh in /inou/models
 importobjtri(filename,struName)
 
 h = plotNormals(struName); % Plots the surface mesh with the normals
+%------------END CODE------------%
