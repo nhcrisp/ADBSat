253dc5162b6f1a6fa377ffd54ffcffbc0640a717
LSinpetru <68545459+LSinpetru@users.noreply.github.com>
2021-03-12 15:15:26 +0000

    Update ADBSat_install.m

--- install/ADBSat_install.m
+++ install/ADBSat_install.m
@@ -1,15 +1,34 @@
-function ADBSat_install(input)
-
-%  ADBSat instalation
+% ADBSat instalation file.
+% Adds the relative paths of the toolbox to the MATLAB path.
 %
-%    Adds the relative paths of the toolbox to the MATLAB path.
-%
-%    input:
+% Inputs:
 %      '' (empty) : installs the toolbox
 %      'uninstall': uninstalls the toolbox
 %
-%*********************************************************************
+% Outputs:
+%       None.
+%
+%--- Copyright notice ---%
+% Copyright (C) 2021 The University of Manchester
+% Written by David Mostaza Prieto,  Nicholas H. Crisp, Luciana Sinpetru and Sabrina Livadiotti
+%
+% This file is part of the ADBSat toolkit.
+%
+% This program is free software: you can redistribute it and/or modify it
+% under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or (at
+% your option) any later version.
+%
+% This program is distributed in the hope that it will be useful, but
+% WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+% Public License for more details.
+%
+% You should have received a copy of the GNU General Public License along
+% with this program. If not, see <http://www.gnu.org/licenses/>.
+%--------- BEGIN CODE -----------%
 
+function ADBSat_install(input)
 
 if nargin == 0
     install = 1;
@@ -55,3 +74,4 @@ elseif ~install % Uninstall toolbox
     disp('ADBSat uninstalled successfully')
     
 end
+%------- END CODE --------%
