7dc87642813b3db4368497c0da32f5df0773eb66
LSinpetru <68545459+LSinpetru@users.noreply.github.com>
2021-03-12 17:02:22 +0000

    Update importobjtri.m

--- toolbox/import/importobjtri.m
+++ toolbox/import/importobjtri.m
@@ -1,5 +1,5 @@
 function [ pathSav ] = importobjtri( fileIn, struName, verb )
-%IMPORTOBJTRI Imports a triangular mesh from a .obj file
+% Imports a triangular mesh from a .obj file
 %
 % Inputs:
 %       fileIn     : input filepath
@@ -13,6 +13,24 @@ function [ pathSav ] = importobjtri( fileIn, struName, verb )
 % The University of Manchester
 % September 2012
 %
+%--- Copyright notice ---%
+% Copyright (C) 2021 The University of Manchester
+% Written by David Mostaza Prieto,  Nicholas H. Crisp, Luciana Sinpetru and Sabrina Livadiotti
+%
+% This file is part of the ADBSat toolkit.
+%
+% This program is free software: you can redistribute it and/or modify it
+% under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or (at
+% your option) any later version.
+%
+% This program is distributed in the hope that it will be useful, but
+% WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+% Public License for more details.
+%
+% You should have received a copy of the GNU General Public License along
+% with this program. If not, see <http://www.gnu.org/licenses/>.
 %------------- BEGIN CODE --------------
 
 [V,F,X,Y,Z,M] = obj_fileTri2patch(fileIn);
