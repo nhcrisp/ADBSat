eab9c3cdc7a5c7eff6d9d9f31836681806820574
LSinpetru <68545459+LSinpetru@users.noreply.github.com>
2021-03-12 15:23:22 +0000

    Update environment.m

--- toolbox/calc/environment.m
+++ toolbox/calc/environment.m
@@ -1,5 +1,4 @@
-function [ vinf, rhoTot, s, Rmean, Talt ] = environment( h, lat, lon, dayOfYear, UTseconds, f107Average, f107Daily, magneticIndex )
-%ENVIRONMENT Computes a number of atmospheric parameters using the MATLAB atmosnrlmsise00 function
+% Computes a number of atmospheric parameters using the MATLAB atmosnrlmsise00 function.
 % Assumes a circular orbit without atmospheric co-rotation or atmospheric winds.
 %
 % Inputs:
@@ -23,7 +22,27 @@ function [ vinf, rhoTot, s, Rmean, Talt ] = environment( h, lat, lon, dayOfYear,
 % The University of Manchester
 % October 2018
 %
+%--- Copyright notice ---%
+% Copyright (C) 2021 The University of Manchester
+% Written by David Mostaza Prieto,  Nicholas H. Crisp, Luciana Sinpetru and Sabrina Livadiotti
+%
+% This file is part of the ADBSat toolkit.
+%
+% This program is free software: you can redistribute it and/or modify it
+% under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or (at
+% your option) any later version.
+%
+% This program is distributed in the hope that it will be useful, but
+% WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+% Public License for more details.
+%
+% You should have received a copy of the GNU General Public License along
+% with this program. If not, see <http://www.gnu.org/licenses/>.
 %------------- BEGIN CODE -------------
+
+function [ vinf, rhoTot, s, Rmean, Talt ] = environment( h, lat, lon, dayOfYear, UTseconds, f107Average, f107Daily, magneticIndex )
 
 % Constants
 [data] = astrophysicalConstants;
