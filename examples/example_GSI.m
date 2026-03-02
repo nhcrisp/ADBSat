66f92d4eef27cf3de619f51ee2dee5cc3afcdcbd
Jav <78877309+faeryjav@users.noreply.github.com>
2026-03-02 14:00:45 +0000

    Update Examples
    Update of example_GSI and example_sphere files
    
    Co-Authored-By: Nicholas Crisp <40802736+nhcrisp@users.noreply.github.com>

--- examples/example_GSI.m
+++ examples/example_GSI.m
@@ -1,41 +1,75 @@
-%%
-GSI_model1 = @coeff_DRIA;
+% Comparison of GSI Example
+%
+%--- Copyright notice ---%
+% Copyright (C) 2021 The University of Manchester
+% Written by David Mostaza Prieto,  Nicholas H. Crisp, Luciana Sinpetru and Sabrina Livadiotti
+%
+% This file is part of the ADBSat toolkit.
+%
+% This program is free software: you can redistribute it and/or modify it
+% under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or (at
+% your option) any later version.
+%
+% This program is distributed in the hope that it will be useful, but
+% WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+% Public License for more details.
+%
+% You should have received a copy of the GNU General Public License along
+% with this program. If not, see <http://www.gnu.org/licenses/>.
+%------------ BEGIN CODE ----------% 
+%% Set up 
+clc 
+clear 
+
+% Load the two gas-surface interaction model functions
+GSI_model1 = @coeff_sentman;
 GSI_model2 = @coeff_CLL;
 
-h = 300e3;
-lat = 0;
-lon = 0;
-dayOfYear = 1;
-UTseconds = 0;
-f107Average = 140;
-f107Daily = 140;
-magneticIndex = ones([1,7])*15;
-
+% Setting up the solver parameters manually
 param_eq = struct;
-param_eq = environment(param_eq, h, lat, lon, dayOfYear, UTseconds, f107Average, f107Daily, magneticIndex, 1);
 
-param_eq.Tw = 300;
-delta = deg2rad(0:1:90); % Angle from Normal
+% Set up the orbital and environmental variables
+h = 300e3; % Altitude [m]
+lat = 0;   % Geodetic Latitude [deg]
+lon = 0;   % Longitude [deg]
+dayOfYear = 1; % Day of the Year [1-365] 
+UTseconds = 0; % Seconds of the day [s]
+f107Average = 140; % Solar Activtiy 
+f107Daily = 140;   
+magneticIndex = ones([1,7])*15; % Geomagnetic Activity 
+AnO = 1; % Flag for anomalous oxygen
 
-param_eq.alpha = 1;
-param_eq.gamma = cos(delta);
-param_eq.ell = sin(delta);
+% Incidence Angle to the flow (for a flat plate) [rad]
+delta = deg2rad(0:1:90); 
+
+%% Main Calculations
+% Calculate the environmental variables
+param_eq = environment(param_eq, h, lat, lon, dayOfYear, UTseconds, f107Average, f107Daily, magneticIndex, AnO);
+
+% Set up of the Sentman model
+param_eq.alpha = 1; % Accommodation Coefficient [-] 
+param_eq.Tw = 300; % Wall temperature [K]
+% Calculate the coefficients using Sentman GSI
 [cp1, ctau1, cd1, cl1] = GSI_model1(param_eq, delta);
 
-% CLL
-param_eq.alphaN = 0.5;
-param_eq.sigmaT = 0.5;
+% Set up CLL model
+param_eq.alphaN = 1; % Normal Accom Coefficient [-] 
+param_eq.sigmaT = 1; % Tangential Accom Coefficient [-] 
+% Calculate the coefficients using CLL GSI
 [cp2, ctau2, cd2, cl2] = GSI_model2(param_eq, delta);
 
+%% Post Processing 
+% Plotting the Cd and Cl distribution
 figure
-figuresize(18, 9, 'cm')
 hold on
 p_1 = plot(delta*(180/pi),cd1);
 set(p_1, 'Color', 'k', 'LineStyle','--', 'LineWidth',1.25)
 p_2 = plot(delta*(180/pi),cd2);
 set(p_2, 'Color', 'k', 'LineStyle','-','LineWidth',1.25)
 grid on
-ylabel('Drag Coefficient, $C_D$')
+ylabel('Drag Coefficient, C_D')
 
 yyaxis right
 set(gca, 'YColor',[0.5 0.5 0.5])
@@ -44,9 +78,9 @@ p_3 = plot(delta*(180/pi),cl1);
 set(p_3, 'Color', [0.5 0.5 0.5], 'LineStyle','--','LineWidth',1.25)
 p_4 = plot(delta*(180/pi),cl2);
 set(p_4, 'Color', [0.5 0.5 0.5], 'LineStyle','-','LineWidth',1.25)
-ylabel('Lift Coefficient, $C_L$')
+ylabel('Lift Coefficient, C_L')
 
 xlabel('Incidence Angle [deg]')
-legend('$C_D$ DRIA ($\alpha = 1$)', '$C_D$ CLL ($\alpha_N = \alpha_T = 0.5$)', '$C_L$ DRIA ($\alpha = 1$)', '$C_L$ CLL ($\alpha_N = \alpha_T = 0.5$)')
+legend('C_D Sentman (\alpha = 1)', 'C_D CLL (\alpha_N = \sigma_T = 1)', 'C_L Sentman (\alpha = 1)', 'C_L CLL (\alpha_N = \sigma_T = 1)')
 
 set(gcf,'color','w');
--- examples/example_sphere.m
+++ examples/example_sphere.m
@@ -1,4 +1,4 @@
-% Cube Example
+% Sphere Example
 %
 %--- Copyright notice ---%
 % Copyright (C) 2021 The University of Manchester
@@ -19,37 +19,45 @@
 % You should have received a copy of the GNU General Public License along
 % with this program. If not, see <http://www.gnu.org/licenses/>.
 %------------ BEGIN CODE ----------%
-
+%% Set up 
 clear
+clc 
 
-modName = 'sphere';
-% Path to model file
+% Path to model file and to result file
+modName = 'sphere'; % model name 
 ADBSat_path = ADBSat_dynpath;
 modIn = fullfile(ADBSat_path,'inou','obj_files',[modName,'.obj']);
 modOut = fullfile(ADBSat_path,'inou','models');
 resOut = fullfile(ADBSat_path,'inou','results',modName);
 
-%Input conditions
-alt = 200; %km
-inc = 51.6; %deg
+% Input orbital conditions
+alt = 200; % orbital altitude [km]
+inc = 51.6; % orbital inclination [deg]
+% Environment conditions 
 env = [alt*1e3, inc/2, 0, 106, 0, 65, 65, ones(1,7)*3, 0]; % Environment variables
-
+% Attitude conditions 
 aoa_deg = 10; % Angle of attack [deg]
 aos_deg = 10; % Angle of sideslip [deg]
 
-% Model parameters
+% Flag for shading algorithm
 shadow = 1;
-inparam.gsi_model = 'cook';
-inparam.accom_model = 'SESAM'; % Accommodation (Goodman model)
+
+% Gas surface interaction model parameters
+inparam.gsi_model = 'sentman';
+inparam.alpha = 1; % Accommodation Coefficient [-]
 inparam.Tw = 300; % Wall Temperature [K]
 
+% Flag for solar radiation model
 solar = 1;
+% Solar Radiation Model parameters
 inparam.sol_cR = 0.15; % Specular Reflectivity
 inparam.sol_cD = 0.25; % Diffuse Reflectivity
 
-verb = 0;
+% Flags for verbose mode and delete temp files
+verb = 1;
 del = 0;
 
+%% Main Calculations 
 % Import model
 [modOut] = ADBSatImport(modIn, modOut, verb);
 
@@ -59,6 +67,7 @@ inparam = environment(inparam, env(1),env(2),env(3),env(4),env(5),env(6),env(7),
 % Coefficient Calculation
 fileOut = calc_coeff(modOut, resOut, deg2rad(aoa_deg), deg2rad(aos_deg), inparam, shadow, solar, 1, 0); 
 
+%% Post processing 
 % Plot surface distribution
 if verb && ~del
     plot_surfq(fileOut, modOut, aoa_deg(1), aos_deg(1), 'cp');
